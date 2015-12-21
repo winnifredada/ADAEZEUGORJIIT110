349af037a0af49042efbdc6803daa00fcdafffc2
System Administrator <root@AdaezesMacBook-Pro.local>
2015-12-21 14:47:30 -0500

    Fifth Tutorial Finished   # In this part I was writing a program that was I used to test my code.     A problem was found when the test was run so i created a test that would     help me expose this problem, so it could be fixed. I created a     file called tests.py and then went back to models.py to fix the problems     that was found while running the test and it came out good. I also created     another question that was part of ther testing.
         Reported-by:Adaeze Ugorji
         Signed-off-by:Adaeze Ugorji<adaezeugorji>

--- polls/models.py
+++ polls/models.py
@@ -9,11 +9,8 @@ class Question(models.Model):
     def __str__(self):
        return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
